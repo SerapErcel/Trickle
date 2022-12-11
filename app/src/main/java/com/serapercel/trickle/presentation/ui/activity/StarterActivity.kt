463f0e07a062604f2efc7895197e6362c1c32168
Serap <ercelserap@gmail.com>
2022-12-12 01:19:26 +0300

    Add splash fragment

---
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/activity/StarterActivity.kt
@@ -0,0 +1,12 @@
+package com.serapercel.trickle.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serapercel.trickle.R
+
+class StarterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starter)
+    }
+}
