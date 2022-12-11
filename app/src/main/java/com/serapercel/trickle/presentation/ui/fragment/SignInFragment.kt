cb41b1ed05d3477e116434addaa33a024d93ef57
Serap <ercelserap@gmail.com>
2022-12-12 02:29:00 +0300

    Add third onboarding screen

--- app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/SignInFragment.kt
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/SignInFragment.kt
@@ -1,4 +1,4 @@
-package com.serapercel.trickle.presentation.ui.onboarding
+package com.serapercel.trickle.presentation.ui.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.serapercel.trickle.R
 
-class WiewPagerFragment : Fragment() {
+class SignInFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wiew_pager, container, false)
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
 }
