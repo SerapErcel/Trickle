5d69697339010aaaf0f6880ff59f6c681bd2fe6d
Serap <ercelserap@gmail.com>
2022-12-12 02:28:32 +0300

    Add second onboarding screen

---
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/onboarding/screens/SecondOnboardingFragment.kt
@@ -0,0 +1,38 @@
+package com.serapercel.trickle.presentation.ui.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.serapercel.trickle.R
+import com.serapercel.trickle.databinding.FragmentFirstOnboardingBinding
+import com.serapercel.trickle.databinding.FragmentSecondOnboardingBinding
+
+class SecondOnboardingFragment : Fragment() {
+
+    private var _binding: FragmentSecondOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewPager2 = activity?.findViewById<ViewPager2>(R.id.viewPager2)
+
+        binding.btnSecondNext.setOnClickListener {
+            viewPager2?.currentItem = 2
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
