0937b628a8051222b6e2b8d0cfc44c880efc5488
Serap <ercelserap@gmail.com>
2022-12-14 00:46:29 +0300

    Add sign up fragment design

---
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package com.serapercel.trickle.presentation.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serapercel.trickle.R
+
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+}
