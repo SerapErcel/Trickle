4187236e3841eff3794788e82a52e1da2a53292f
Serap <ercelserap@gmail.com>
2023-01-15 01:54:06 +0300

    Add overview card design

---
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/MainOverviewFragment.kt
@@ -0,0 +1,26 @@
+package com.serapercel.trickle.presentation.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serapercel.trickle.databinding.FragmentMainOverviewBinding
+
+class MainOverviewFragment : Fragment() {
+    private var _binding: FragmentMainOverviewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainOverviewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
