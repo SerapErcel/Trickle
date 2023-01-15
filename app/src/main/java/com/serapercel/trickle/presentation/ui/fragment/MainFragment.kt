6ff6f0849299a160de0d992b67e6dd820f79497a
Serap <ercelserap@gmail.com>
2023-01-15 22:03:12 +0300

    Update main fragment design

--- app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/serapercel/trickle/presentation/ui/fragment/MainFragment.kt
@@ -6,17 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.serapercel.trickle.R
+import com.serapercel.trickle.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        replaceFragment(R.id.fragmentContainerOverview, MainOverviewFragment())
+        replaceFragment(R.id.fragmentContainerNeeds, NeedsOverviewFragment())
+        replaceFragment(R.id.fragmentContainerCurrency, CurrencyFragment())
+        replaceFragment(R.id.fragmentContainerTransactions, LastTransactionsFragment())
 
 
+    }
+    private fun replaceFragment(container: Int,fragment: Fragment) {
+        val manager = requireActivity().supportFragmentManager
+        val fragmentTransaction = manager.beginTransaction()
+        fragmentTransaction.replace(container, fragment)
+        fragmentTransaction.commit()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
